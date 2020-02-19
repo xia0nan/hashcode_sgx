0cabfa6eed8361b7e3322cf8888f072c83917702
Nan <xnone0104@gmail.com>
2020-02-19 09:47:30 +0800

    Add assertions according to requirements

--- practice_round/01_brute_force.py
+++ practice_round/01_brute_force.py
@@ -17,25 +17,37 @@ file_names = [
 ]
 
 def main():
-
-    # read example a
+    # read line by line
     with open(file_names[0], 'r') as f:
         content = f.readlines()
     # remove '\n'
-    content = [x.strip() for x in content] 
+    content = [x.strip() for x in content]
 
     # get M and N
     m, n = [int(x) for x in content[0].split(" ")]
+    assert 1 <= m <= 10e9
+    assert 1 <= n <= 10e5
 
     # Get S0, S1, ..., Sn-1
     s_list = [int(x) for x in content[1].split(" ")]
 
-    # for output
+    # list is ascending
+    assert sorted(s_list) == s_list
+
+    # last one less or equal than m
+    assert s_list[-1] <= m
+
+    # final output file
     line1 = -1
     line2 = []
 
     # assume at least 2 components
     assert max(s_list) + min(s_list) < m
+    
+    # check all combinations
+    for i in reversed(range(2, n+1)):
+        check_list = list(combinations(s_list, i))
+        # print(check_list)
 
     # find max sum under m
     max_sum = 0
@@ -49,7 +61,7 @@ def main():
             if sum(j) > max_sum and sum(j) < m:
                 max_sum = sum(j)
                 max_list = j
-                
+
     # get output lines
     line1 = len(max_list)
     line2 = [s_list.index(x) for x in max_list]
@@ -59,7 +71,6 @@ def main():
         f.write(str(line1))
         f.write('\n')
         f.write(' '.join(map(str, line2)))
-    
-    
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
