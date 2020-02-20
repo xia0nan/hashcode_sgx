5fc9f42d35479ec06b8adb6501d02b2a0c71b7f1
Nan Xiao <xnone0104@gmail.com>
2020-02-21 05:08:18 +0800

    add naive solution

--- qualification_round/main.py
+++ qualification_round/main.py
@@ -2,6 +2,15 @@ import glob
 
 import parsr, solver, scorer, writer
 
+file_names = [
+              "a_example.txt",
+              "b_read_on.txt",
+              "c_incunabula.txt",
+              "d_tough_choices.txt",
+              "e_so_many_books.txt",
+              "f_libraries_of_the_world.txt"
+]
+
 for idx, filename in enumerate(glob.glob('data/*')):
     dataset = parsr.parse(filename)
     solution = solver.solve(dataset)
--- qualification_round/solver.py
+++ qualification_round/solver.py
@@ -1,2 +1,22 @@
+"""
+Naive solution:
+Rank library and books by natural order
+"""
 def solve(dataset):
-	return [0]
+    accu_signup_days = 0
+    max_dates = dataset['days']
+    libs = []
+    for lib in dataset["libraries"]:
+        if accu_signup_days < max_dates:
+            item = dict()
+            item['id'] = lib['id']
+            item['signup_order'] = lib['id']
+            item['num_book'] = lib['lib_num_books']
+            item['process_days'] = lib['process_days']
+            item['books_order'] = lib['lib_books']
+            libs.append(item)
+
+    solution = dict()
+    solution['num_libraries'] = len(libs)
+    solution['libraries'] = libs
+    return solution
