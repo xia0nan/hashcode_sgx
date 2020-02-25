3b4cbef984998406cb72ff6b4530682889d2d331
Nan Xiao <xnone0104@gmail.com>
2020-02-26 00:57:48 +0800

    add all solutions in solver

--- practice_round/main.py
+++ practice_round/main.py
@@ -4,7 +4,7 @@ import parser, solver, scorer, writer
 
 for idx, filename in enumerate(glob.glob('datasets/*')):
     dataset = parser.parse(filename)
-    solution = solver.solve(dataset)
+    solution = solver.solvemc(dataset)
     score = scorer.score(solution, dataset)
     print('Score for %s: %s/%s (%s to perfect score)' % (filename, score, dataset['knapsize'], dataset['knapsize'] - score))
     writer.write(solution, filename.replace('datasets','solutions'))
--- practice_round/solver.py
+++ practice_round/solver.py
@@ -1,2 +1,63 @@
+# # 01. Naive Solution
+# def solve(dataset):
+# 	return [0]
+
+# 02. Greedy Solution
 def solve(dataset):
-	return [0]
+	slices = 0
+	solution = []
+	for i in range(len(dataset['pizzas'])):
+		if slices + dataset['pizzas'][i] <= dataset['knapsize']:
+			solution.append(i)
+	return solution
+
+# 03. Solving the knapsack
+def knapsolve(dataset):
+    maxscore = dataset['knapsize']
+    dp = [None for _ in range(maxscore+1)]
+    dp[0] = []
+    for idx, item in progressbar.progressbar(list(enumerate(dataset['pizzas']))):
+        for i in range(maxscore - item, -1, -1):
+            if dp[i] is None: continue            
+            dp[i+item] = dp[i] + [idx]
+    i = -1
+    while dp[i] is None: i -= 1
+    return dp[i]
+
+# 04. Optimizing the knapsack : partial reconstruction
+def rebuild(solution, dataset):
+    newsol = list(solution)
+    for i in range(min(len(newsol),3)):
+        idx = random.randrange(len(newsol))
+        print(idx, dataset['pizzas'][newsol[idx]])
+        del newsol[idx]
+    newscore = scorer.score(newsol, dataset)
+    remaining = dataset['knapsize'] - newscore
+    st = set(newsol)
+    newpizz = []
+    for i in range(len(dataset['pizzas'])):
+        if i in st:
+			# Pizzas already used in the solution are replaced with infinite slices
+			# so we avoid selecting them twice while keeping the list indices correct
+            newpizz.append(99**99)
+        else:
+            newpizz.append(dataset['pizzas'][i])
+    print('rem', remaining)
+    reco = knapsolve({'pizzas':newpizz, 'knapsize':remaining})
+    return sorted(newsol + reco)
+
+# 05. Upping the ante
+import random
+def solvemc(dataset):
+    capa = dataset['knapsize']
+    sol = []
+    for i in range(len(dataset['pizzas'])-1, -1, -1):
+        if random.getrandbits(2) and capa >= dataset['pizzas'][i]:
+            sol.append(i)
+            capa -= dataset['pizzas'][i]
+    st = set(sol)
+    for i in range(len(dataset['pizzas'])-1, -1, -1):
+        if i not in st and capa >= dataset['pizzas'][i]:
+            sol.append(i)
+            capa -= dataset['pizzas'][i]
+    return sol
